a3e2ba4783bbd492674e5a199bbf734e0bf46e1a
davidkelly-wk <david.kelly@workiva.com>
2020-11-14 13:33:05 -0700

    Updated producer and created consumer v2

---
+++ average_sentiment_consumerV2.py
@@ -0,0 +1,57 @@
+from kafka import KafkaConsumer
+from json import loads
+from sentiment import *
+
+class AverageSentimentConsumer():
+    def __init__(self, topic):
+        self.consumer = KafkaConsumer(
+            topic,
+            bootstrap_servers=['localhost:9092'],
+            auto_offset_reset='earliest',
+            enable_auto_commit=True,
+            auto_commit_interval_ms =  5000,
+            fetch_max_bytes = 128,
+            max_poll_records = 100,
+            group_id=None,
+            value_deserializer=lambda x: loads(x.decode('utf-8')))
+        self.sentiment_analyzer = Sentiment()
+
+    def calculate_sentiment_score(self, message):
+        score = float('inf') # in case message not in english
+        if 'text' in message:
+            if message['lang'] == 'en':
+                score = self.sentiment_analyzer.score_text(message['text'])
+                print('Message score: {} for message: {}'.format(score, message['text']))
+        return score
+
+    def average_sentiment(self, consumer):
+        i = 0
+        avg_score = float(0)
+        for message in consumer:
+            message = message.value
+            score = self.calculate_sentiment_score(message)
+            if score != float('inf'): # if 'inf' something went wrong calculating the score
+                if avg_score != float(0): # if this is the first message, just add it rather than average it
+                    avg_score = (avg_score + score) / 2
+                else: 
+                    avg_score = score
+            print('Avg score: {} after message {}'.format(avg_score, i))
+            i += 1
+            if i > 10:
+                break
+        return i, avg_score
+        
+
+    def start_consumer(self):
+        while True:
+            fetched_records, avg_score = self.average_sentiment(self.consumer)
+            print('Records fetched: {} with avg. score : {}'.format(fetched_records, avg_score))
+        self.average_sentiment(self.consumer)
+
+if __name__ == "__main__":
+    topic = input('Enter a kafka topic name: ')
+    run_average_consumer = input('Do you wish to run the sentiment averaging consumer (y/n)? ')
+
+    if run_average_consumer == 'y':
+        asc = AverageSentimentConsumer(topic)
+        asc.start_consumer()
--- twitter_stream_producer.py
+++ twitter_stream_producer.py
@@ -1,32 +1,42 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
-from kafka import KafkaProducer, KafkaClient
-from sentiment import *
+from kafka import KafkaProducer
 import json
 
-class TwitterStreamProducer:
-    def __init__(self, topic):
-        access_token = "1323451448781238274-ARnzAE9Jr9T4KjiruQSqIJM4WnAiCk"
-        access_token_secret =  "KfMietdsdxqRqKiEVVEJdKypfGbrWjFaE9Y92Y1ij4ilk"
-        consumer_key =  "doz9ZltVGwMmlg2PTptGcoMLd"
-        consumer_secret =  "0BrrMlwj443opgxpjkowqJ18vqrwLnHUaY1eooYTvL4xZmtfFJ"
-        kafka = KafkaClient()
-        producer = KafkaProducer()
-        l = StdOutListener(kafka, producer, topic)
+# Twitter API access keys
+access_token = "1323451448781238274-ARnzAE9Jr9T4KjiruQSqIJM4WnAiCk"
+access_token_secret =  "KfMietdsdxqRqKiEVVEJdKypfGbrWjFaE9Y92Y1ij4ilk"
+consumer_key =  "doz9ZltVGwMmlg2PTptGcoMLd"
+consumer_secret =  "0BrrMlwj443opgxpjkowqJ18vqrwLnHUaY1eooYTvL4xZmtfFJ"
+# Topic and producer
+topic = "default_topic"
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):
         auth = OAuthHandler(consumer_key, consumer_secret)
         auth.set_access_token(access_token, access_token_secret)
-        stream = Stream(auth, l)
-        stream.filter(track="twitter")
+        return auth
 
-class StdOutListener(StreamListener):
-    def __init__(self, kafka, producer, topic):
-        self.kafka = kafka
-        self.producer = producer
-        self.topic = topic
 
+class TwitterStreamProducer():
+    def __init__(self, topic_name):
+        global topic
+        topic = topic_name
+        self.twitter_authenticator = TwitterAuthenticator()
+
+    def stream_tweets(self):
+        while True:
+            listener = TwitterStreamListener()
+            auth = self.twitter_authenticator.authenticate_twitter_app()
+            stream = Stream(auth, listener)
+            stream.filter(track="twitter", stall_warnings=True, languages= ["en"])
+        
+
+class TwitterStreamListener(StreamListener):
     def on_data(self, data):
-        self.producer.send(self.topic, data.encode('utf-8'))
+        producer.send(topic, data.encode('utf-8'))
         json_obj = json.loads(data)
         if 'text' in json_obj:
             if json_obj['lang'] == 'en':
@@ -35,3 +45,10 @@ class StdOutListener(StreamListener):
 
     def on_error(self, status):
         print (status)
+
+if __name__ == "__main__":
+    topic = input('Enter a kafka topic name: ')
+    run_producer = input('Do you wish to run the twitter producer (y/n)? ')
+    if run_producer == 'y':
+        tsp = TwitterStreamProducer(topic)
+        tsp.stream_tweets()

6ff2cc6dcbb4467cd8af7f9b17d9192f05f02423
davidkelly-wk <david.kelly@workiva.com>
2020-11-17 13:54:26 -0700

    Re-added sentiment analysis

--- average_sentiment_consumerV2.py
+++ average_sentiment_consumerV2.py
@@ -18,41 +18,35 @@ class AverageSentimentConsumer():
         score = float('inf') # in case message not in english
         if 'text' in message:
             if message['lang'] == 'en':
-                score = self.sentiment_analyzer.score_text(message['text'])
-                print('Message score: {} for message: {}'.format(score, message['text']))
+                message = self.sentiment_analyzer.remove_url(message['text'])
+                score = self.sentiment_analyzer.score_text(message)
+                print('Message score: {} for message: {}'.format(score, message))
         return score
 
     def average_sentiment(self, consumer):
-        i = 0
+        i = 1
         avg_score = float(0)
-        for message in consumer:
-            message = message.value
-            score = self.calculate_sentiment_score(message)
-            if score != float('inf'): # if 'inf' something went wrong calculating the score
-                if avg_score != float(0): # if this is the first message, just add it rather than average it
-                    avg_score = (avg_score + score) / 2
-                else: 
-                    avg_score = score
-            print('Avg score: {} after message {}'.format(avg_score, i))
+        msg_pack = self.consumer.poll(timeout_ms=10000)
+        for tp, messages in msg_pack.items():
+            print(len(msg_pack.items()))
+            print(len(messages))
+            for message in messages:
+                message = message.value
+                score = self.calculate_sentiment_score(message)
+                if score != float('inf'): # if 'inf' something went wrong calculating the score
+                    if avg_score != float(0): # if this is the first message, just add it rather than average it
+                        avg_score = (avg_score + score) / 2
+                    else: 
+                        avg_score = score
+                print('Avg score: {} after message {}'.format(avg_score, i))
+                i += 1
         return i, avg_score
         
 
     def start_consumer(self):
         while True:
-            # Response format is {TopicPartiton('topic1', 1): [msg1, msg2]}
-            msg_pack = self.consumer.poll(timeout_ms=10000)
-            for tp, messages in msg_pack.items():
-                print(len(msg_pack.items()))
-                print(len(messages))
-            # message value and key are raw bytes -- decode if necessary!
-            # e.g., for unicode: `message.value.decode('utf-8')`
-                for message in messages:
-                    print ("%s:%d:%d: key=%s value=%s" % (tp.topic, tp.partition,
-                                                    message.offset, message.key,
-                                                    message.value))
-            #fetched_records, avg_score = self.average_sentiment(self.consumer)
-            #print('Records fetched: {} with avg. score : {}'.format(fetched_records, avg_score))
-        #self.average_sentiment(self.consumer)
+            fetched_records, avg_score = self.average_sentiment(self.consumer)
+            print('Records fetched: {} with avg. score : {}'.format(fetched_records, avg_score))
 
 if __name__ == "__main__":
     topic = input('Enter a kafka topic name: ')
